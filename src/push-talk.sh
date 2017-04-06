f416fb55cafbecd47acbf820269cde986d93c2ea
Jens Barthel <jens.barthel@gmail.com>
2017-04-06 23:26:59 +0200

    add script to push talk data to talk.sigdevops.io

---
+++ src/push-talk.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Building proxy talk nginx..."
+wget https://github.com/jensbarthel/proxy-talk/archive/master.zip
+unzip master.zip
+rm master.zip
+cd proxy-talk-master
+docker build -t registry.sigdevops.io/talk-nginx
+echo "Pushing proxy talk nginx..."
+docker push registry.sigdevops.io/talk-nginx
+
+echo "Starting proxy talk nginx..."
+docker run -d \
+    --name sigdevops-talk \
+    -e "VIRTUAL_HOST=talk.sigdevops.io" \
+    -e "LETSENCRYPT_HOST=talk.sigdevops.io" \
+    -e "LETSENCRYPT_EMAIL=jb@wps.de" \
+    registry.sigdevops.io/talk-nginx
